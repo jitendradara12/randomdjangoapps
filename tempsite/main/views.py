8db58748bfa73ac316baaac8d474b5565e4d8230
sastauser <jitendradara4464@gmail.com>
2025-07-15 23:56:40 +0530

    first app

--- tempsite/main/views.py
+++ tempsite/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(responce):
+    return HttpResponse("<h1>hellllllllllllllo<h1>")
