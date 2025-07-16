87a223bdcfd600b15f429e77af4185e48f6df672
sastauser <jitendradara4464@gmail.com>
2025-07-16 12:35:56 +0530

    testing workflow by push

--- tempsite/main/views.py
+++ tempsite/main/views.py
@@ -7,4 +7,4 @@ from . import models
 def index(responce, id):
     ls = models.ToDolist.objects.get(id=id)
     item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><p>%s</p>" % (ls, str(item.text)))
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls, str(item.text)))
